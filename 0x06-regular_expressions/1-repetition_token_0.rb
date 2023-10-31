e1942497890b0e0b521e7ed06dd08bd223b3703a
yoyo71212 <yusufswelam2@gmail.com>
2023-10-31 08:24:21 -0700

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
-# Check if the argument matches "hbt?n" using Oniguruma regex
+# Check if the argument matches "hbt*n" using Oniguruma regex
 if ARGV.length == 1
   input = ARGV[0]
-  regex = /hbt?n/
+  regex = /hbt*n/
   matches = input.scan(regex)
 
   if matches.empty?
