ef4d94676649e545d518b23ea5bfc952a1f8c04c
yoyo71212 <yusufswelam2@gmail.com>
2023-10-31 08:35:33 -0700

    3-repetition_token_2.rb

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -3,12 +3,13 @@
 if ARGV.length == 1
   input = ARGV[0]
   regex = /hbt{1,4}n/
-  matches = input.scan(regex)
+#  matches = input.scan(regex)
+  puts input.scan(regex).join
 
-  if matches.empty?
-    puts ""
-  else
-    all_matches = matches.join
-    puts all_matches
-  end
+#  if matches.empty?
+#    puts ""
+#  else
+#    all_matches = matches.join
+#    puts all_matches
+#  end
 end
