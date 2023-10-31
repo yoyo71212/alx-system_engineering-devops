7a32c8b8e328c8ca8a059e8f8c9bbdcdb44a4f19
yoyo71212 <yusufswelam2@gmail.com>
2023-10-31 09:12:30 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,5 @@
 #!/usr/bin/env ruby
 # Textme
 input = ARGV[0]
-regex = /^(.*?),(.*?),(.*?)$/
-matches = input.scan(regex)
-
-sender = matches[1]
-receiver = matches[2]
-flags = matches[3]
-
-puts "sender: #{sender}"
-puts "receiver: #{receiver}"
-puts "flags: #{flags}"
+regex = /from:(.*?)\],to:(.*?)\],flags:(.*?)\]/
+input.scan(regex).join
