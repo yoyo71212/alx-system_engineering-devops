16def0a919b86cae8c6c65f2e1f1ce039706bbf2
Yusuf Swelam <yusufswelam2@gmail.com>
2024-01-05 14:53:54 -0800

    3-repetition_token_2.rb

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# Check if the argument matches "hbt{1,5}n" using Oniguruma regex
+if ARGV.length == 1
+  input = ARGV[0]
+  regex = /hbt{1,5}n/
+#  matches = input.scan(regex)
+  puts input.scan(regex).join
+
+#  if matches.empty?
+#    puts ""
+#  else
+#    all_matches = matches.join
+#    puts all_matches
+#  end
+end
